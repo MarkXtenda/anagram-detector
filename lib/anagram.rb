08879fb36f3dcca3c5cf2595b84dd97fa43fd0a4
MarkXtenda <mark.i2p.1998@gmail.com>
2022-08-09 20:02:34 +1000

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram 
+    attr_accessor :anagram
+
+    def initialize(anagram)
+        @anagram = anagram.split("").sort
+    end
+    def match(anagram_match)
+        anagram_array = []
+        anagram_match.each do |string|
+            (string.split("").sort == anagram) ? anagram_array << string : nil
+        end
+        anagram_array
+    end
+end
